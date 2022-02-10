6ba1787c0bfa8182047638219cbb4ef26a749c00
Juan Antonio <juan.cahuana@globant.com>
2022-02-10 17:11:08 -0500

    First commit

---
+++ src/context/shopContext.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Client from "shopify-buy";
+
+export class shopContext extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default shopContext;
